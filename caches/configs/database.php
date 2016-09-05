17bf4d9780b3b8796c842b9b327fd426cc735a7f
missliaoboy <917601716@qq.com>
2016-09-05 22:51:37 +0800

    Update database.php

--- caches/configs/database.php
+++ caches/configs/database.php
@@ -1,32 +1,20 @@
 <?php
 
 return array (
-		'default' => array (
-		'hostname' => '113.107.150.47',
+
+	'default' => array (
+		'hostname' => '127.0.0.1',
 		'port' => 3306,
-		'database' => 'lishi',
-        'username' => 'webDBAdmin',
-        'password' => 'webAdmin@2015-12!',
-        'tablepre' => 'lishi_',
+		'database' => 'lishi1',
+		'username' => 'root',
+		'password' => 'root',
+		'tablepre' => 'lishi_',
 		'charset' => 'utf8',
 		'type' => 'mysql',
 		'debug' => true,
 		'pconnect' => 0,
 		'autoconnect' => 0
-		),
-		// 'default' => array (
-		// 'hostname' => '127.0.0.1',
-		// 'port' => 3306,
-		// 'database' => 'lishi1',
-  //       'username' => 'root',
-  //       'password' => 'root',
-  //       'tablepre' => 'lishi_',
-		// 'charset' => 'utf8',
-		// 'type' => 'mysql',
-		// 'debug' => true,
-		// 'pconnect' => 0,
-		// 'autoconnect' => 0
-		// ),
+	),
 );
 
 ?>
